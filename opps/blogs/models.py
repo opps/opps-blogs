575fbb90399b3a6d7981ffe85d54a4e888f51458
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-06-12 16:49:40 -0300

    Create basic model BlogPost

--- opps/blogs/models.py
+++ opps/blogs/models.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from opps.articles.models import Article
 
 
-class Blog(models.Model):
-    pass
+class BlogPost(Article):
+    content = models.TextField(_(u"Content"))
+    albums = models.ManyToManyField(
+        'articles.Album',
+        null=True, blank=True,
+        related_name='blogpoast_albums',
+        verbose_name=_(u"Albums")
+    )
+
+    class Meta:
+        verbose_name = _(u'Blog post')
+        verbose_name_plural = _(u'Blog Posts')
+
+    def get_absolute_url(self):
+        return u"/blog/{}/{}/{}".format(self.user.username,
+                                        self.channel_long_slug, self.slug)
