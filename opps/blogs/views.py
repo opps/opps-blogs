2fc4f9e6483203bc7d87faff32d789a0ab6e8c0c
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-06-12 23:13:21 -0300

    Create PostBlogDetail on view

--- opps/blogs/views.py
+++ opps/blogs/views.py
@@ -7,6 +7,7 @@ from opps.articles.views.generic import OppsDetail, OppsList
 
 from .models import PostBlog
 
+
 class PostBlogList(OppsList):
     model = PostBlog
     type = "blogs"
@@ -20,6 +21,25 @@ class PostBlogList(OppsList):
         self.article = self.model.objects.filter(
             site_domain=self.site,
             user__useraname=self.kwargs['user__useraname'],
+            date_available__lte=timezone.now(),
+            published=True)
+        return self.article
+
+
+class PostBlogDetail(OppsDetail):
+    model = PostBlog
+    type = "blogs"
+    channel_long_slug = []
+    channel = None
+
+    @property
+    def queryset(self):
+        self.site = get_current_site(self.request).domain
+        self.long_slug = None
+        self.article = self.model.objects.filter(
+            site_domain=self.site,
+            user__useraname=self.kwargs['user__useraname'],
+            slug=self.kwargs['slug'],
             date_available__lte=timezone.now(),
             published=True)
         return self.article
