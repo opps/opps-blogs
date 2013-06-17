d3ace9595b7935cec1762565933babf014fec0d0
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-06-17 20:47:42 -0300

    Add Blog model, user is ManyToManyField

--- opps/blogs/models.py
+++ opps/blogs/models.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from django.db import models
+from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 
+from opps.core.models import NotUserPublishable, Slugged
 from opps.articles.models import Article
+
+
+class Blog(NotUserPublishable, Slugged):
+
+    user = models.ManyToManyField(settings.AUTH_USER_MODEL)
+    name = models.CharField(_(u"Name"), max_length=140)
 
 
 class BlogPost(Article):
