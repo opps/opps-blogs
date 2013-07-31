f253e704943cba7f207fc3b74b29d5ba42adc5ae
Bruno Rocha <rochacbruno@gmail.com>
2013-07-31 15:16:43 -0300

    Removed Redactor on OppsBlogs

--- opps/blogs/forms.py
+++ opps/blogs/forms.py
@@ -4,10 +4,10 @@ from django import forms
 
 from .models import BlogPost
 
-from redactor.widgets import RedactorEditor
+from opps.core.widgets import OppsEditor
 
 
 class BlogPostAdminForm(forms.ModelForm):
     class Meta:
         model = BlogPost
-        widgets = {'content': RedactorEditor()}
+        widgets = {'content': OppsEditor()}
