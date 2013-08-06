2f6317b2e4477f7093fa6180a730586c74af4154
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-08-05 23:35:38 -0300

    Use dynamic url via app conf

--- opps/blogs/urls.py
+++ opps/blogs/urls.py
@@ -1,21 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, url
-from django.conf import settings
 from django.views.decorators.cache import cache_page
 
 from .views import BlogPostList, BlogPostDetail, BlogList
+from .conf import settings
 
 
 urlpatterns = patterns(
     '',
-    url(r'^(?P<blog__slug>[\w-]+)/authors/$',
+    url(r'^{}/(?P<blog__slug>[\w-]+)/authors$'.format(
+        settings.OPPS_BLOGS_CHANNEL),
         cache_page(settings.OPPS_CACHE_EXPIRE)(BlogList.as_view()),
-        name='blogpost-detail'),
-    url(r'^(?P<blog__slug>[\w-]+)/(?P<slug>[\w-]+)/$',
+        name='blogpost-detail',
+        kwargs={'channel__long_slug': settings.OPPS_BLOGS_CHANNEL}
+    ),
+    url(r'^{}/(?P<channel__long_slug>[\w//-]+)/(?P<slug>[\w-]+)$'.format(
+        settings.OPPS_BLOGS_CHANNEL),
         cache_page(settings.OPPS_CACHE_EXPIRE)(BlogPostDetail.as_view()),
-        name='blogpost-detail'),
-    url(r'^(?P<blog__slug>[\w-]+)/$',
+        name='blogpost-detail',
+        kwargs={'channel__long_slug': settings.OPPS_BLOGS_CHANNEL}
+    ),
+    url(r'^{}/(?P<blog__slug>[\w\b//-]+)/$'.format(settings.OPPS_BLOGS_CHANNEL),
         cache_page(settings.OPPS_CACHE_EXPIRE)(BlogPostList.as_view()),
-        name='blogpost-list'),
+        name='blogpost-list',
+        kwargs={'channel__long_slug': settings.OPPS_BLOGS_CHANNEL}
+    ),
 )
