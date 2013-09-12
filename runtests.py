f325156b86a67c339c57d15d65f17a875195362f
Ellison Leao <ellisonleao@gmail.com>
2013-09-11 23:49:45 -0300

    Fix runtests

--- runtests.py
+++ runtests.py
@@ -31,7 +31,6 @@ if not settings.configured:
             'opps.core',
             'opps.channels',
             'opps.boxes',
-            'opps.sources',
             'opps.articles',
             'opps.images',
             'opps.sitemaps',
