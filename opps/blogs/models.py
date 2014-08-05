057cdbfe278171c15fb93466689b863d1749b058
Jean O. Rodrigues <jeanmaskpl@gmail.com>
2014-08-05 15:24:26 -0300

    Fix pep8 issues and add unicode_literals.

--- opps/blogs/models.py
+++ opps/blogs/models.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -20,17 +22,17 @@ from .conf import settings
 
 class Category(MPTTModel, NotUserPublishable, Slugged):
     blog = models.ForeignKey('blogs.Blog', related_name='categories')
-    name = models.CharField(_(u"Name"), max_length=140)
-    long_slug = models.SlugField(_(u"Path name"), max_length=250,
+    name = models.CharField(_("Name"), max_length=140)
+    long_slug = models.SlugField(_("Path name"), max_length=250,
                                  db_index=True)
-    show_in_menu = models.BooleanField(_(u"Show in menu?"), default=False)
-    group = models.BooleanField(_(u"Group sub-channel?"), default=False)
-    order = models.IntegerField(_(u"Order"), default=0)
+    show_in_menu = models.BooleanField(_("Show in menu?"), default=False)
+    group = models.BooleanField(_("Group sub-channel?"), default=False)
+    order = models.IntegerField(_("Order"), default=0)
     parent = TreeForeignKey(
         'self',
         related_name='subchannel',
         null=True, blank=True,
-        verbose_name=_(u'Parent'),
+        verbose_name=_('Parent'),
         limit_choices_to={
             'parent__isnull': True, # We don't allow subsubcategories
         }
@@ -50,11 +52,11 @@ class Category(MPTTModel, NotUserPublishable, Slugged):
         http://en.wikipedia.org/wiki/Uniform_resource_identifier
         """
         if self.parent:
-            return u"/{}/{}/".format(self.parent.slug, self.slug)
-        return u"/{}/".format(self.slug)
+            return "/{}/{}/".format(self.parent.slug, self.slug)
+        return "/{}/".format(self.slug)
 
     def get_absolute_url(self):
-        return u"/{}/{}{}".format(settings.OPPS_BLOGS_CHANNEL,
+        return "/{}/{}{}".format(settings.OPPS_BLOGS_CHANNEL,
                                   self.blog.slug, self.__unicode__())
 
     @property
@@ -63,7 +65,7 @@ class Category(MPTTModel, NotUserPublishable, Slugged):
 
     def _make_long_slug(self):
         if self.parent:
-            return u'{}/{}'.format(self.parent.slug, self.slug)
+            return '{}/{}'.format(self.parent.slug, self.slug)
         return self.slug
 
     def clean(self):
@@ -83,7 +85,8 @@ class Category(MPTTModel, NotUserPublishable, Slugged):
             if self.pk:
                 category_list = category_list.exclude(pk=self.pk)
             if category_list.exists():
-                raise ValidationError(_('The slug chosen already exists. Please try another.'))
+                raise ValidationError(
+                    _('The slug chosen already exists. Please try another.'))
         finally:
             super(Category, self).clean()
 
@@ -100,26 +103,27 @@ class Category(MPTTModel, NotUserPublishable, Slugged):
 
 class Blog(NotUserPublishable, Slugged):
     LAYOUT_MODES = (
-        ('default', _(u'Default')),
-        ('resumed', _(u'Resumed')),
-        ('mix', _(u'Mix')),
+        ('default', _('Default')),
+        ('resumed', _('Resumed')),
+        ('mix', _('Mix')),
     )
 
     user = models.ManyToManyField(settings.AUTH_USER_MODEL,
-                                  verbose_name=_(u'Users'))
-    name = models.CharField(_(u"Name"), max_length=140)
-    main_image = models.ForeignKey(Image, verbose_name=_(u'Main Image'),
+                                  verbose_name=_('Users'))
+    name = models.CharField(_("Name"), max_length=140)
+    main_image = models.ForeignKey(Image, verbose_name=_('Main Image'),
                                    blank=True, null=True)
-    description = models.TextField(_(u'Description'), blank=True)
-    type = models.CharField(_(u'Blog Type'), max_length=200,
+    description = models.TextField(_('Description'), blank=True)
+    type = models.CharField(_('Blog Type'), max_length=200,
                             choices=settings.OPPS_BLOGS_TYPES)
-    layout_mode = models.CharField(_(u'Layout mode'), max_length=200,
+    layout_mode = models.CharField(_('Layout mode'), max_length=200,
                                    default='default', choices=LAYOUT_MODES)
 
-    __unicode__ = lambda self: self.name
+    def __unicode__(self):
+        return self.name
 
     def get_absolute_url(self):
-        return u"/{}/{}/".format(settings.OPPS_BLOGS_CHANNEL, self.slug)
+        return "/{}/{}/".format(settings.OPPS_BLOGS_CHANNEL, self.slug)
 
     def get_profile(self):
         if not settings.OPPS_BLOGS_PROFILE:
@@ -156,28 +160,28 @@ class Blog(NotUserPublishable, Slugged):
         return self.categories.filter(published=True, show_in_menu=True)
 
     class Meta:
-        verbose_name = _(u'Blog')
-        verbose_name_plural = _(u'Blogs')
+        verbose_name = _('Blog')
+        verbose_name_plural = _('Blogs')
         ordering = ('name', )
 
 
 class BlogPost(Article):
-    blog = models.ForeignKey('blogs.Blog', verbose_name=_(u'Blog'))
-    content = models.TextField(_(u"Content"))
+    blog = models.ForeignKey('blogs.Blog', verbose_name=_('Blog'))
+    content = models.TextField(_("Content"))
     category = models.ForeignKey('blogs.Category', blank=True, null=True,
-                                 verbose_name=_(u'Category'))
+                                 verbose_name=_('Category'))
     albums = models.ManyToManyField(
         'articles.Album',
         null=True, blank=True,
         related_name='blogpoast_albums',
-        verbose_name=_(u"Albums")
+        verbose_name=_("Albums")
     )
     videos = models.ManyToManyField(Video, blank=True, null=True,
                                     through='BlogPostVideo')
     audios = models.ManyToManyField(Audio, blank=True, null=True,
                                     through='BlogPostAudio')
 
-    accept_comments = models.BooleanField(_(u'Accept comments?'),
+    accept_comments = models.BooleanField(_('Accept comments?'),
                                           default=True)
 
     related_blogposts = models.ManyToManyField(
@@ -189,8 +193,8 @@ class BlogPost(Article):
 
     class Meta:
         ordering = ['-date_available']
-        verbose_name = _(u'Blog post')
-        verbose_name_plural = _(u'Blog Posts')
+        verbose_name = _('Blog post')
+        verbose_name_plural = _('Blog Posts')
         get_latest_by = 'date_available'
 
     def get_absolute_url(self):
@@ -200,14 +204,14 @@ class BlogPost(Article):
         except AttributeError:
             slug = 'sem-categoria'
 
-        return u"/{}/{}/{}/{}.html".format(settings.OPPS_BLOGS_CHANNEL,
+        return "/{}/{}/{}/{}.html".format(settings.OPPS_BLOGS_CHANNEL,
                                            self.blog.slug, slug, self.slug)
 
 class BlogPostRelated(models.Model):
 
     blogpost = models.ForeignKey(
         'blogs.BlogPost',
-        verbose_name=_(u'BlogPost'),
+        verbose_name=_('BlogPost'),
         null=True,
         blank=True,
         related_name='blogpostrelated_blogpost',
@@ -215,13 +219,13 @@ class BlogPostRelated(models.Model):
     )
     related = models.ForeignKey(
         'blogs.BlogPost',
-        verbose_name=_(u'Related BlogPost'),
+        verbose_name=_('Related BlogPost'),
         null=True,
         blank=True,
         related_name='blogpostrelated_related',
         on_delete=models.SET_NULL
     )
-    order = models.PositiveIntegerField(_(u'Order'), default=0)
+    order = models.PositiveIntegerField(_('Order'), default=0)
 
     class Meta:
         verbose_name = _('Related content')
@@ -230,45 +234,45 @@ class BlogPostRelated(models.Model):
 
     def __unicode__(self):
         if self.related and self.blogpost:
-            return u"{0}->{1}".format(self.related.slug, self.blogpost.slug)
+            return "{0}->{1}".format(self.related.slug, self.blogpost.slug)
         return None
 
 
 class BlogLink(NotUserPublishable):
     blog = models.ForeignKey('blogs.Blog', related_name='links')
-    name = models.CharField(_(u"Name"), max_length=140)
+    name = models.CharField(_("Name"), max_length=140)
     link = models.URLField(_('Link'))
 
-    __unicode__ = lambda self: u"{} - {}".format(self.name, self.link)
+    __unicode__ = lambda self: "{} - {}".format(self.name, self.link)
 
     class Meta:
-        verbose_name = _(u'Blog Link')
-        verbose_name_plural = _(u'Blog Links')
+        verbose_name = _('Blog Link')
+        verbose_name_plural = _('Blog Links')
 
 
 class BlogPostVideo(models.Model):
     blogpost = models.ForeignKey('blogs.BlogPost', null=True, blank=True,
-                                 verbose_name=_(u'Blog'),
+                                 verbose_name=_('Blog'),
                                  on_delete=models.SET_NULL)
     video = models.ForeignKey(Video, null=True, blank=True,
-                              verbose_name=_(u'Video'),
+                              verbose_name=_('Video'),
                               on_delete=models.SET_NULL)
 
     class Meta:
-        verbose_name = _(u'Blogpost Video')
-        verbose_name_plural = _(u'Blogpost Videos')
+        verbose_name = _('Blogpost Video')
+        verbose_name_plural = _('Blogpost Videos')
 
 
 class BlogPostAudio(models.Model):
     blogpost = models.ForeignKey('blogs.BlogPost', null=True, blank=True,
-                                 verbose_name=_(u'Blog'),
+                                 verbose_name=_('Blog'),
                                  on_delete=models.SET_NULL)
-    audio = models.ForeignKey(Audio, verbose_name=_(u'Audio'), null=True,
+    audio = models.ForeignKey(Audio, verbose_name=_('Audio'), null=True,
                               blank=True, on_delete=models.SET_NULL)
 
     class Meta:
-        verbose_name = _(u'Blogpost Audio')
-        verbose_name_plural = _(u'Blogpost Audios')
+        verbose_name = _('Blogpost Audio')
+        verbose_name_plural = _('Blogpost Audios')
 
 
 @receiver(post_save, sender=Blog)
